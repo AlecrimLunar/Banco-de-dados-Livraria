cfa5a16499bc7fc4ba816ade340069a81399ee4b
Lutero <luterogoulart@gmail.com>
2024-03-02 20:25:59 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("oi");
+        System.out.println("LOGIN NO SISTEMA \n\nUsuário:");
+        Scanner tc = new Scanner(System.in);
+        long id = Long.parseLong(tc.nextLine());
+    }
+
+    public static Connection criaConexao(){
+        try {
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/livraria" +
+                    "?user=lutero&password=123456&ssl=true");
+        }catch (Exception e){
+            System.out.println("Falha na conexão com o banco de dados");
+        }
+        return null;
     }
 }
