9ffb0d08cb7f5b717ac2ebb48706690ee49f41c0
Lutero <luterogoulart@gmail.com>
2024-03-02 22:52:53 -0300

    inicio do sistema ta +- feito, tem algumas funções do controla já implementadas

--- src/ControlaBD.java
+++ src/ControlaBD.java
@@ -2,15 +2,70 @@ import java.sql.*;
 
 public class ControlaBD {
 
-    Connection cnn;
+    private Connection con;
 
     public ControlaBD(){
+        String dbURL = "jdbc:postgresql://localhost:5432/livraria";
+        String login = "lutero";
+        String password = "123456";
         try {
-            cnn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/livraria" +
-                    "?user=lutero&password=123456&ssl=true");
+
+            con = DriverManager.getConnection(dbURL, login, password);
+
         }catch (Exception e){
-            System.out.println("Falha na conexão com o banco de dados");
+            System.out.println("Falha na conexão com o banco de dados: " + e);
             System.exit(1);
         }
+    }
+
+    public boolean Insert(String tabela, String infos){
+        try{
+            Statement st = con.createStatement();
+            String consulta = "INSERT INTO " + tabela + " VALUES (" + infos + ");";
+
+            int a = st.executeUpdate(consulta);
+            if (a == 1)
+                return true;
+        }catch(Exception e){
+            System.out.println("ERRO - INSERT: " + e);
+        }
+        return false;
+    }
+
+    public boolean Existe(String pesquisa, String tabela, String coluna){
+        try {
+            Statement st = con.createStatement();
+            String consulta = "SELECT * FROM " + tabela + " WHERE " + coluna + " = " + pesquisa + ";";
+
+            ResultSet rt = st.executeQuery(consulta);
+
+            /*se rt for nulo, nada foi retornado da consulta, logo não existe nada no
+             * banco de dados com o valor pesquisado, então retorna false*/
+            return (rt != null);
+        }catch (Exception e){
+            System.out.println("ERRO - QUERRY: " + e);
+            return false;
+        }
+    }
+
+    public int Quantos(String pesquisa, String tabela){
+        try{
+
+            /*caso não seja especificado um campo para procurar, sera realizada a consulta
+            * utilizando o *, o que significa que ele ira contar todas as linhas da tabela*/
+
+            if (pesquisa.isEmpty())
+                pesquisa = "*";
+
+            Statement st = con.createStatement();
+            String consulta = "SELECT * count(" + pesquisa + ") FROM " + tabela + ";";
+
+            ResultSet rt = st.executeQuery(consulta);
+            return (Integer.parseInt(rt.toString()));
+
+        }catch (Exception e){
+            System.out.println("ERRO - QUERRY: " + e);
+        }
+        return -1;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,62 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         ControlaBD controle = new ControlaBD();
-
-        System.out.println("LOGIN NO SISTEMA \n\nUsuário:");
         Scanner tc = new Scanner(System.in);
+        IniciaSistema(controle, tc);
+    }
+
+    public static void IniciaSistema(ControlaBD controle, Scanner tc){
+
+        if (controle.Quantos("", "vendedor") == 0){
+            System.out.println("-----------------------------------------------------------------------------" +
+                    "BEM VINDO AO SISTEMA! NENHUM REGISTRO DE VENDEDORES FOI ENCONTRADO." +
+                    "PARA UTILIZAR O SISTEMA \nÉ NECESSÁRIO SER UM VENDEDOR. DESEJA CADASTRAR UM" +
+                    "NOVO VENDEDOR?\nATENÇÃO: É NECESSÁRIO UM VENDEDOR PARA UTILIZAR O SISTEMA, CASO " +
+                    "NÃO SEJA CADASTRADO UM NOVO VENDEDOR O SISTEMA IRÁ DESLIGAR!");
+
+            if (tc.nextLine().equalsIgnoreCase("sim")){
+                while (true) {
+                    System.out.print("MUITO BEM, INSIRA AS SEGUINTES INFORMAÇÕES:\nNome: ");
+                    String nome = tc.nextLine();
+
+                    System.out.print("\nCFP (Apenas números): ");
+                    long CPF = Long.parseLong(tc.nextLine());
+
+                    System.out.print("\nSenha de acesso: ");
+                    String senha = tc.nextLine();
+
+                    System.out.println("\n\nMUITO BEM, VERIFIQUE SE AS INFORMAÇÕES ESTÃO CORRETAS. SE SIM DIGITE " +
+                            "'Sim', SE NÃO DIGITE 'Não'\nNome: " + nome + "\nCPF: " + CPF + "\n");
+
+                    if (tc.nextLine().equalsIgnoreCase("sim")) {
+                        String insert = "'" + nome + "', '" + senha + "', " + CPF;
+                        if (controle.Insert("vendedor", insert)) {
+                            System.out.println("CADASTRO CONCLUÍDO COM SUCESSO! PARA LOGAR, UTILIZE O ID: E A" +
+                                    "SENHA INFORMADA." +
+                                    "---------------------------------------------------------------------");
+                            break;
+                        }
+                    }
+                }
+
+            } else{
+                System.exit(0);
+            }
+        }
+        System.out.print("LOGIN NO SISTEMA\n" +
+                "-------------------------------------------------" +
+                " \nUsuário: ");
+
         long id = Long.parseLong(tc.nextLine());
+        System.out.print("\nSenha: ");
+
+        /*não tenho ideia de como fazer isso aqui, acho que vamos precisar de duas chamadas
+        * do select, uma pra pegar os usuários com aquele nome e outra pra pegar eles com a senha.
+        * talvez a gente possa fazer isso de forma diferente pq n tem pq da select ja q a gente so
+        * quer saber se tem ou não*/
+
     }
 }
