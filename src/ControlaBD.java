c6a542c1028d06d2fe69ec70633de261cf1efc80
AlecrimLunar <129176336+AlecrimLunar@users.noreply.github.com>
2024-03-03 17:58:22 -0300

    Adição de login e dois metodos de update

--- src/ControlaBD.java
+++ src/ControlaBD.java
@@ -6,8 +6,8 @@ public class ControlaBD {
 
     public ControlaBD(){
         String dbURL = "jdbc:postgresql://localhost:5432/livraria";
-        String login = "lutero";
-        String password = "123456";
+        String login = "alecrim";
+        String password = "21092004nicolas";
         try {
 
             con = DriverManager.getConnection(dbURL, login, password);
@@ -40,7 +40,7 @@ public class ControlaBD {
             ResultSet rt = st.executeQuery(consulta);
 
             /*se rt for nulo, nada foi retornado da consulta, logo não existe nada no
-             * banco de dados com o valor pesquisado, então retorna false*/
+             banco de dados com o valor pesquisado, então retorna false*/
             return (rt != null);
         }catch (Exception e){
             System.out.println("ERRO - QUERRY: " + e);
@@ -52,20 +52,71 @@ public class ControlaBD {
         try{
 
             /*caso não seja especificado um campo para procurar, sera realizada a consulta
-            * utilizando o *, o que significa que ele ira contar todas as linhas da tabela*/
+             * utilizando o *, o que significa que ele ira contar todas as linhas da tabela*/
 
             if (pesquisa.isEmpty())
                 pesquisa = "*";
 
             Statement st = con.createStatement();
-            String consulta = "SELECT * count(" + pesquisa + ") FROM " + tabela + ";";
+            String consulta = "SELECT COUNT(" + pesquisa + ") FROM " + tabela + ";";
 
             ResultSet rt = st.executeQuery(consulta);
-            return (Integer.parseInt(rt.toString()));
+            return rt.next() ? rt.getInt(1) : -1;
+            /*Nico: tive que adicionar a linha acima pq tava dando erro aqui. basicamente onde eu
+             * posso botar .next eu coloco pra funcionar*/
 
         }catch (Exception e){
             System.out.println("ERRO - QUERRY: " + e);
         }
         return -1;
+    }
+
+    /*Nico:tive que criar pra fazer o login e ta funfando certinho, só add a coluna usuario na
+     * tua tabela visse*/
+    public boolean login(String user, String password){
+        try {
+            Statement st = con.createStatement();
+            String consulta = "SELECT senha FROM vendedor WHERE usuario = '" + user + "';";
+
+            ResultSet rt = st.executeQuery(consulta);
+
+            rt.next();
+            /*esse next ta ajeitando o "ponteiro" pra pegar a string. Sabe o index de quando se
+             * lê um arquivo .txt? então aquele bagulho lá*/
+            return password.equalsIgnoreCase(rt.getString(1));
+        }catch (Exception e){
+            System.out.println("ERRO - QUERRY: " + e);
+            return false;
+        }
+    }
+
+    public boolean alter(String tabela, String coluna,String novo, String antigo){
+        try{
+            Statement st = con.createStatement();
+            String consulta = "UPDATE " + tabela + " SET " + coluna + " = '" + novo + "' WHERE " + coluna + " '" + antigo + "';";
+
+            int a = st.executeUpdate(consulta);
+            if(a == 1)
+                return true;
+        } catch (Exception e){
+            System.out.println("ERRO - UPDATE: " + e);
+        }
+        return false;
+    }
+    /*Nico: Esses são os UPDATES, como até agora a gente só tem string e long da pra deixar assim.
+     * to usando o bagulho de overwrite ou sobre-escrita. Onde só mudando oq a função recebe o programa escolhe a certa
+     * só pude fazer isso hoje... amanha (segunda) pego nesse cod de novo*/
+    public boolean alter(String tabela, String coluna, long novo, long antigo){
+        try{
+            Statement st = con.createStatement();
+            String consulta = "UPDATE " + tabela + " SET " + coluna + " = '" + novo + "' WHERE " + coluna + " '" + antigo + "';";
+
+            int a = st.executeUpdate(consulta);
+            if(a == 1)
+                return true;
+        } catch (Exception e){
+            System.out.println("ERRO - UPDATE: " + e);
+        }
+        return false;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -12,13 +12,15 @@ public class Main {
     public static void IniciaSistema(ControlaBD controle, Scanner tc){
 
         if (controle.Quantos("", "vendedor") == 0){
-            System.out.println("-----------------------------------------------------------------------------" +
+            System.out.println("-----------------------------------------------------------------------------\n" +
                     "BEM VINDO AO SISTEMA! NENHUM REGISTRO DE VENDEDORES FOI ENCONTRADO." +
                     "PARA UTILIZAR O SISTEMA \nÉ NECESSÁRIO SER UM VENDEDOR. DESEJA CADASTRAR UM" +
                     "NOVO VENDEDOR?\nATENÇÃO: É NECESSÁRIO UM VENDEDOR PARA UTILIZAR O SISTEMA, CASO " +
-                    "NÃO SEJA CADASTRADO UM NOVO VENDEDOR O SISTEMA IRÁ DESLIGAR!");
+                    "NÃO SEJA CADASTRADO UM NOVO VENDEDOR O SISTEMA IRÁ DESLIGAR!\n" +
+                    "-----------------------------------------------------------------------------");
+            //dei uma formatação nesse print pra ficar bonitinho :D
 
-            if (tc.nextLine().equalsIgnoreCase("sim")){
+            if (tc.nextLine().equalsIgnoreCase("sim") || tc.nextLine().equalsIgnoreCase("s")){
                 while (true) {
                     System.out.print("MUITO BEM, INSIRA AS SEGUINTES INFORMAÇÕES:\nNome: ");
                     String nome = tc.nextLine();
@@ -26,14 +28,20 @@ public class Main {
                     System.out.print("\nCFP (Apenas números): ");
                     long CPF = Long.parseLong(tc.nextLine());
 
+                    System.out.print("\nNome de acesso: ");
+                    String user = tc.nextLine();
+                    //adicionei o nome de acesso pra fazer o login de forma mais facil. Do q pelo nome completo
+
                     System.out.print("\nSenha de acesso: ");
                     String senha = tc.nextLine();
+
 
                     System.out.println("\n\nMUITO BEM, VERIFIQUE SE AS INFORMAÇÕES ESTÃO CORRETAS. SE SIM DIGITE " +
                             "'Sim', SE NÃO DIGITE 'Não'\nNome: " + nome + "\nCPF: " + CPF + "\n");
 
                     if (tc.nextLine().equalsIgnoreCase("sim")) {
-                        String insert = "'" + nome + "', '" + senha + "', " + CPF;
+                        String insert = "DEFAULT, '" + nome + "', '" + senha + "', " + CPF + ", '" + user + "'";
+                        //como ce pode ver o nome de usuario fica no final ->-->-->-->-->-->-->-->-->-^
                         if (controle.Insert("vendedor", insert)) {
                             System.out.println("CADASTRO CONCLUÍDO COM SUCESSO! PARA LOGAR, UTILIZE O ID: E A" +
                                     "SENHA INFORMADA." +
@@ -48,16 +56,29 @@ public class Main {
             }
         }
         System.out.print("LOGIN NO SISTEMA\n" +
-                "-------------------------------------------------" +
-                " \nUsuário: ");
-
-        long id = Long.parseLong(tc.nextLine());
-        System.out.print("\nSenha: ");
+                "-------------------------------------------------\n");
 
         /*não tenho ideia de como fazer isso aqui, acho que vamos precisar de duas chamadas
-        * do select, uma pra pegar os usuários com aquele nome e outra pra pegar eles com a senha.
-        * talvez a gente possa fazer isso de forma diferente pq n tem pq da select ja q a gente so
-        * quer saber se tem ou não*/
+         * do select, uma pra pegar os usuários com aquele nome e outra pra pegar eles com a senha.
+         * talvez a gente possa fazer isso de forma diferente pq n tem pq da select ja q a gente so
+         * quer saber se tem ou não*/
+
+        /*Nico: Ta resolvido meu querido :D*/
+        do {
+            System.out.print("Usuário: ");
+
+            String nome = tc.nextLine();
+            System.out.print("\nSenha: ");
+            String senha = tc.nextLine();
+
+            if (controle.login(nome, senha)) {
+                System.out.print("\nLOGIN EFETUADO COM SUCESSO!");
+                break;
+            } else {
+                System.out.print("SENHA OU USUÁRIO INCORRETO! \nTENTE NOVAMENTE\n\n");
+            }
+        }while(true);
+
 
     }
 }
