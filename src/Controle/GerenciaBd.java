d3a20f4411af084d8fcfeddbb331e635f1a24e5a
Lutero <luterogoulart@gmail.com>
2024-04-28 00:03:17 -0300

    o hash de gerenciaBd agr eh final; correção da função recuperaComprasNaoConfirmadas

--- src/Controle/GerenciaBd.java
+++ src/Controle/GerenciaBd.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
  */
 public abstract class GerenciaBd implements AutoCloseable{
 
-    private HashMap<String, String> qualNomeTabelaBanco = null;
+    private static final HashMap<String, String> qualNomeTabelaBanco = null;
     private Connection connection;
     private int usuarioBanco;
 
@@ -205,6 +205,7 @@ public abstract class GerenciaBd implements AutoCloseable{
      * @return uma string contendo a nova operação.
      */
     private String montaCondicao(String condicao, String tabela){
+
         if (!tabela.equalsIgnoreCase("carrinho_livro") &&
                 !tabela.equalsIgnoreCase("relatorio_venda")) {
             if (condicao.isEmpty())
@@ -1013,49 +1014,53 @@ public abstract class GerenciaBd implements AutoCloseable{
      */
     protected ResultSet recuperaComprasNaoConfirmadas()
             throws NaoTemConexaoException, ConexaoException {
-        String tabela = "Compras_Info.compra";
-        String coluna = "forma_pagamento, data, valor";
 
-        try {
-            return Select(coluna, tabela, "id_vendedor = -1 AND id_compra >= 0",
-                    connection);
-        } catch (SQLException e) {
+        if (connection != null) {
+            String tabela = "Compras_Info.compra";
+            String coluna = "forma_pagamento, data, valor";
+
+            try {
+                return Select(coluna, tabela, "id_vendedor = -1 AND id_compra >= 0",
+                        connection);
+            } catch (SQLException e) {
+                /*
+                 * Se der erro, vai tentar fechar a conexão atual.
+                 */
+                try {
+                    close();
+
+                } catch (SQLException f) {
+                    /*
+                     * Se não conseguir ele informa
+                     * a quem o chamou que não foi possível
+                     * encerrar a conexão com o banco e que
+                     * ela é uma conexão defeituosa.
+                     */
+                    throw new ConexaoException();
+                }
+                try {
+                    /*
+                     * O sistema tentar criar outra conexão.
+                     */
+
+                    criaCon(usuarioBanco);
+                } catch (SQLException f) {
+                    /*
+                     * Caso ele não consiga, é necessário avisar
+                     * que existe um grave problema: não existe conexão
+                     * com o banco de dados.
+                     */
+                    throw new NaoTemConexaoException();
+                }
+            }
             /*
-             * Se der erro, vai tentar fechar a conexão atual.
+             * Caso tenha sido possível resolver os erros, a função avisa que ela
+             * teve um comportamento inesperado e foi possível resolver ele,
+             * por isso ela pode ser chamada novamente.
              */
-            try {
-                close();
-
-            } catch (SQLException f) {
-                /*
-                 * Se não conseguir ele informa
-                 * a quem o chamou que não foi possível
-                 * encerrar a conexão com o banco e que
-                 * ela é uma conexão defeituosa.
-                 */
-                throw new ConexaoException();
-            }
-            try {
-                /*
-                 * O sistema tentar criar outra conexão.
-                 */
-
-                criaCon(usuarioBanco);
-            } catch (SQLException f) {
-                /*
-                 * Caso ele não consiga, é necessário avisar
-                 * que existe um grave problema: não existe conexão
-                 * com o banco de dados.
-                 */
-                throw new NaoTemConexaoException();
-            }
+            return null;
         }
-        /*
-         * Caso tenha sido possível resolver os erros, a função avisa que ela
-         * teve um comportamento inesperado e foi possível resolver ele,
-         * por isso ela pode ser chamada novamente.
-         */
-        return null;
+        throw new NaoTemConexaoException();
     }
 
     protected void setUsuarioBanco(int usuarioBanco) {
